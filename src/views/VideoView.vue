8ef2c94b71eddf5bed757ad69d0312f9b557a215
svetlana <fatinialux@yandex.ru>
2023-07-25 14:51:51 +0400

    feat: add video

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -4,7 +4,7 @@
     <h2 class="video__title">Фишки из CSS</h2>
     <p class="video__subtitle">Предлагаю твоему вниманию короткое видео, чтобы познакомиться с удивительными возможностями CSS</p>
     <div class="video__iframes">
-      <iframe class="video__iframe" src="https://www.youtube.com/embed/5MgBikgcWnY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/sSwy0Gee1rY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     <div class="content__link content__link_type_green">
       <a href="https://github.com/SvetlanaPivovarova/kids-web/archive/refs/heads/main.zip">Скачать репозиторий этого приложения</a>
